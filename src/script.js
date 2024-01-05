886e6083819a844328fef16841bc07ca1e2e23ef
Dmitriy <dmitriy.ananyin.web@gmail.com>
2024-01-05 15:11:36 +0600

    creation of base map with MapBox

--- src/script.js
+++ src/script.js
@@ -2,13 +2,8 @@ const MAP_ID = 'map'
 const INIT_VIEW_COORDS = [55.751244, 37.618423]
 const INIT_VIEW_ZOOM = 13
 
-const OSM_TILE_LAYER_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-const OSM_TILE_LAYER_OPTIONS = {
-  attribution: '@ OpenStreetMap contributors'
-}
+L.mapbox.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
 
-const map = L.map(MAP_ID).setView(INIT_VIEW_COORDS, INIT_VIEW_ZOOM)
-
-const layer = L.tileLayer(OSM_TILE_LAYER_URL, OSM_TILE_LAYER_OPTIONS)
-
-layer.addTo(map)
+const map = L.mapbox.map(MAP_ID)
+  .setView(INIT_VIEW_COORDS, INIT_VIEW_ZOOM)
+  .addLayer(L.mapbox.tileLayer('mapbox/streets'))
