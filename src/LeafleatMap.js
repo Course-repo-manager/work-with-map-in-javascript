789a28efd85a9a567d588e7306412c21210ea4a3
Dmitriy <dmitriy.ananyin.web@gmail.com>
2024-01-06 23:50:55 +0600

    events handling

--- src/LeafleatMap.js
+++ src/LeafleatMap.js
@@ -14,6 +14,14 @@ const DEFAULT_ZOOM_CONTROL_OPTIONS = {
 export default class {
   #map;
 
+  get value () {
+    return this.#map
+  }
+
+  set value (val) {
+    throw new Error('Map value is readonly')
+  }
+
   constructor({
     rootId,
     mapOptions,
@@ -58,5 +66,61 @@ export default class {
 
   addTileLayer(tileLayer) {
     tileLayer?.addTo?.(this.#map)
+  }
+  
+  convertDegrees(lat = '', lon = '') {
+    if (lat.toString().length !== 7 || lon.toString().length !== 7) {
+      lat = Number(lat).toFixed(6);
+      lon = Number(lon).toFixed(6);
+    }
+
+    return {
+      // ГГ.гггггг / DD.dddddd
+      lat: lat.toString(),
+      lon: lon.toString(),
+
+      // ГГ°MM.ммм' / DD°MM.mmm'
+      latDM: this.convertdTo_dm(lat),
+      lonDM: this.convertdTo_dm(lon),
+
+      // ГГ°MM'СС.с" / DD°MM'SS.s"
+      latDMS: this.convertdTo_dms(lat),
+      lonDMS: this.convertdTo_dms(lon),
+    };
+  }
+
+
+  convertdTo_dm(degrees) {
+    let mydegrees, myminutes, remaining;
+
+    if (degrees == '') {
+      mydegrees = '';
+      myminutes = '';
+    } else {
+      mydegrees = parseInt(degrees + '');
+      remaining = Number(degrees) - mydegrees * 1.0;
+      myminutes = remaining * 60.0;
+      myminutes = Math.round(myminutes * 1000.0) / 1000.0;
+    }
+
+    return `${mydegrees}&deg;${myminutes}`;
+  }
+
+  convertdTo_dms(degrees) {
+    let mydegrees, myminutes, myseconds, remaining;
+    if (degrees == '') {
+      mydegrees = '';
+      myminutes = '';
+      myseconds = '';
+    } else {
+      mydegrees = parseInt(degrees + '');
+      remaining = Number(degrees) - mydegrees * 1.0;
+      const minutes = remaining * 60.0;
+      myminutes = parseInt(minutes + '');
+      remaining = minutes - myminutes * 1.0;
+      myseconds = remaining * 60.0;
+      myseconds = Math.round(myseconds * 10.0) / 10.0;
+    }
+    return `${mydegrees}&deg;${myminutes}&rsquo;${myseconds}&quot;`;
   }
 }
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import LeafleatMap from "./LeafleatMap"
-import { TILE_LAYERS_SETTING } from "./TileLayersConfig"
+import LeafleatMap from "./LeafleatMap.js"
+import { TILE_LAYERS_SETTING } from "./TileLayersConfig.js"
 
 const MAP_ID = 'map'
 const INIT_VIEW_COORDS = [55.751244, 37.618423]
@@ -21,3 +21,31 @@ map.addTileLayers(TILE_LAYERS_SETTING)
 map.setZoomRange(MIN_ZOOM, MAX_ZOOM)
 
 map.addZoomControl()
+
+const coordsNode = document.getElementById('map__coords')
+
+map.value.on('click', (e) => {
+  const { lat, lng } = e.latlng
+  const { latDMS, lonDMS } = map.convertDegrees(lat, lng)
+  coordsNode.innerHTML = `Широта: ${latDMS} <br> Долгота: ${lonDMS}.`
+  coordsNode.style.display = 'block'
+})
+
+map.value.on('move', (e) => {
+  const bounds = e.target.getBounds();
+
+  const latLeftTop = bounds.getSouth();
+  const lngLeftTop = bounds.getWest()
+  
+  const latRightBottom = bounds.getNorth()
+  const lngRightBottom = bounds.getEast()
+  
+  const leftTop = map.convertDegrees(latLeftTop, lngLeftTop)
+  const rightBottom = map.convertDegrees(latRightBottom, lngRightBottom)
+
+  coordsNode.innerHTML = (`
+    Левый верхний угол:  ${leftTop.lat}, ${leftTop.lat};<br> 
+    Нижний правый угол: ${rightBottom.lat}, ${rightBottom.lat};
+  `);
+  coordsNode.style.display = 'block'
+})
